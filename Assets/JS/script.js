8920f88a250c6d9bad865c1ebef0e43a6628f919
Aster Oliva <asterthealabaster@gmail.com>
2024-03-10 04:37:34 -0700

    added dynamic classes to time-blocks for past, present, & future

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
-  const today = dayjs();
-  const todayFormat = today.format("dddd, MMMM D");
+  const now = dayjs();
+  const nowFormat = now.format("dddd, MMMM D");
 
   // Append current date to HTML
-  $("p").children("span").text(todayFormat);
+  $("p").children("span").text(nowFormat);
 
   // Save button event listener
   function clickHandler(event) {
@@ -11,13 +11,12 @@ $(document).ready(function () {
 
     if (timeBlock) {
       let hourId = timeBlock.id;
-      console.log(`${hourId} schedule saved`);
+      console.log(`${hourId}-schedule saved`);
 
       // Save user input to local storage
       let text = timeBlock.querySelector("textarea").value;
-      console.log(text);
-
       localStorage.setItem(`${hourId}-schedule`, text);
+      console.log(`${hourId}-schedule updated in localstorage`);
     }
   }
 
@@ -25,15 +24,30 @@ $(document).ready(function () {
     button.addEventListener("click", clickHandler);
   });
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
+  // Apply past / present / future classes to each time-block
+  const hours = $(".time-block");
+
+  for (let i = 0; i < hours.length; i++) {
+    let hoursEl = hours.eq(i);
+
+    checkTime(hoursEl);
+  }
+
+  function checkTime(hoursEl) {
+    let hoursElId = hoursEl.attr("id");
+    let hoursStrings = hoursElId.split("-");
+    let timeTest = hoursStrings[1];
+
+    if (now.isBefore(dayjs().hour(timeTest).valueOf(), "hour")) {
+      hoursEl.addClass("future");
+    } else if (now.isSame(dayjs().hour(timeTest).valueOf(), "hour")) {
+      hoursEl.addClass("present");
+    } else if (now.isAfter(dayjs().hour(timeTest).valueOf(), "hour")) {
+      hoursEl.addClass("future");
+    }
+  }
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
 });
