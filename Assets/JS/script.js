28ad5481e8a01a9c2c1de9416af199e7695cf24d
Aster Oliva <asterthealabaster@gmail.com>
2024-03-14 01:31:30 -0700

    modified script.js to clone & relabel time-block divs, cleaned up redundant code

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,62 +1,63 @@
 $(document).ready(function () {
   const now = dayjs();
-  const nowFormat = now.format("dddd, MMMM D");
 
   // Append current date to HTML
-  $("p").children("span").text(nowFormat);
+  $("#currentDay").children("span").text(now.format("dddd, MMMM D"));
 
-  // Save button event listener
+  // Clone time blocks from template (9AM to 5PM)
+  for (let i = 10; i <= 17; i++) {
+    const container = document.getElementById("scheduleContainer");
+    const template = document.getElementById("hour-09");
+    const clone = template.cloneNode(true);
+
+    // Update cloned time block ids
+    clone.id = `hour-${i.toString().padStart(2, "0")}`;
+
+    // Update cloned time block labels
+    if (i < 12) {
+      clone.querySelector(".hour").textContent = `${i}AM`;
+    } else if (i === 12) {
+      clone.querySelector(".hour").textContent = `${i}PM`;
+    } else {
+      const iFormat = i - 12;
+      clone.querySelector(".hour").textContent = `${iFormat}PM`;
+    }
+
+    container.appendChild(clone);
+  }
+
+  // Save button click handler
   function clickHandler(event) {
     let timeBlock = event.target.closest(".time-block");
 
     if (timeBlock) {
       let hourId = timeBlock.id;
-      console.log(`${hourId}-schedule saved`);
 
       // Save user input to local storage
-      let text = timeBlock.querySelector("textarea").value;
-      localStorage.setItem(`${hourId}-schedule`, text);
-      console.log(`${hourId}-schedule updated in localStorage`);
+      let input = timeBlock.querySelector("textarea").value;
+      localStorage.setItem(`${hourId}-schedule`, input);
+      console.log(`${hourId}-schedule saved to localStorage`);
     }
   }
 
+  // Save button event listener
   document.querySelectorAll(".btn").forEach((button) => {
     button.addEventListener("click", clickHandler);
   });
 
   // Function to compare time blocks to current time
   function checkTime(hoursEl) {
-    let hoursElId = hoursEl.attr("id");
-    let hoursStrings = hoursElId.split("-");
-    let timeTest = hoursStrings[1];
+    const hoursStr = hoursEl.attr("id").split("-")[1];
 
-    // Create 24hr time stamp to pass into dayjs
-    function currentTimeStamp() {
-      // const currentHour = dayjs().hour();
-      const currentMinute = dayjs().minute();
-      const currentSecond = dayjs().second();
-
-      let timeStamp = `${timeTest}:${currentMinute}:${currentSecond}`;
-      return timeStamp;
-    }
-
-    // Convert 24hr time to 12hr time
-    const timeConvertAM = (military) =>
-      military ? dayjs(`1/1/1 ${military}`).format("h:mm A") : null;
-
-    console.log(timeConvertAM(currentTimeStamp()));
-
-    // Check if time blocks are past, present, or future
-    if (now.isBefore(dayjs().hour(timeTest).valueOf(), "hour")) {
+    if (now.isBefore(dayjs().hour(hoursStr).valueOf(), "hour")) {
       hoursEl.addClass("future");
-    } else if (now.isSame(dayjs().hour(timeTest).valueOf(), "hour")) {
+    } else if (now.isSame(dayjs().hour(hoursStr).valueOf(), "hour")) {
       hoursEl.addClass("present");
-    } else if (now.isAfter(dayjs().hour(timeTest).valueOf(), "hour")) {
+    } else if (now.isAfter(dayjs().hour(hoursStr).valueOf(), "hour")) {
       hoursEl.addClass("past");
     }
   }
 
-  // Loop to call checkTime for each time block
   const hours = $(".time-block");
 
   for (let i = 0; i < hours.length; i++) {
@@ -66,15 +67,15 @@ $(document).ready(function () {
   }
 
   // Populate time blocks with user input saved in localStorage
-  const textAreas = document.querySelectorAll("textarea");
-
   function loadStorage() {
+    const textAreas = document.querySelectorAll("textarea");
+
     textAreas.forEach((textarea) => {
       const parentId = textarea.parentNode.id;
-      const scheduleData = localStorage.getItem(`${parentId}-schedule`);
+      const inputData = localStorage.getItem(`${parentId}-schedule`);
 
-      if (scheduleData) {
-        textarea.value = scheduleData;
+      if (inputData) {
+        textarea.value = inputData;
       } else {
         textarea.setAttribute(
           "placeholder",
